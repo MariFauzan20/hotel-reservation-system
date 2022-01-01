6ac69997a79d1277d8f9ebcb2e54ee783618ca7d
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-01-01 18:46:56 +0700

    Add methods for inserting new data

--- src/HotelReservationSystem/Database.java
+++ src/HotelReservationSystem/Database.java
@@ -36,4 +36,90 @@ public class Database {
             e.printStackTrace();
         }
     }
+
+    public void insertCustomer(Customer c) {
+        try {
+            String id = c.getId();
+            String username = c.getUsername();
+            String password = c.getPassword();
+            String nama = c.getNama();
+            
+            String sqlUser = "INSERT INTO user VALUES('%s','%s','%s','%s')";
+            String sqlCustomer = "INSERT INTO customer VALUES('%s','%s','%s')";
+            sqlUser = String.format(sqlUser, id, username, password, "customer");
+            sqlCustomer = String.format(sqlCustomer, id, id, nama);
+            
+            stmt.execute(sqlUser);
+            stmt.execute(sqlCustomer);
+            System.out.println("Customer baru berhasil dibuat.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void insertHotel(Hotel h) {
+        try {
+            String id = h.getId();
+            String username = h.getUsername();
+            String password = h.getPassword();
+            String nama = h.getNama();
+            String deskripsi = h.getDeskripsi();
+            String lokasi = h.getLokasi();
+            int bintang = h.getBanyakBintang();
+            
+            String sqlUser = "INSERT INTO user VALUES('%s','%s','%s','%s')";
+            String sqlHotel = "INSERT INTO hotel VALUES('%s','%s','%s','%s','%s',%d)";
+            sqlUser = String.format(sqlUser, id, username, password, "hotel");
+            sqlHotel = String.format(sqlHotel, id, id, nama, deskripsi, lokasi, bintang);
+            
+            stmt.execute(sqlUser);
+            stmt.execute(sqlHotel);
+            System.out.println("Hotel baru berhasil dibuat.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void insertPilihanMenu(String id_hotel, PilihanMenuKamar p) {
+        try {
+            String id = p.getId();
+            
+            Kamar k = p.getJenisKamar();
+            String id_kamar = k.getId();
+            String tipe = k.getTipe();
+            int harga = k.getHargaPerMalam();
+            int batas_org = k.getBatasOrangPerKamar();
+            
+            int banyak_kamar = p.getbanyakKamar();
+            
+            String sqlKamar = "INSERT INTO kamar VALUES ('%s','%s',%d,%d)";
+            String sqlPM = "INSERT INTO pilihan_menu VALUES('%s','%s','%s',%d)";
+            
+            sqlKamar = String.format(sqlKamar, id_kamar, tipe, harga, batas_org);
+            sqlPM = String.format(sqlPM, id, id_hotel, id_kamar, banyak_kamar);
+            
+            stmt.execute(sqlKamar);
+            stmt.execute(sqlPM);
+            System.out.println("Pilihan menu berhasil ditambah.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void insertDetilPemesanan(DetilPemesanan d) {
+        try {
+            String id = d.getId();
+            String id_c = d.getCustomer().getId();
+            String id_h = d.getHotel().getId();
+            String id_k = d.getTipeKamar().getId();
+            int byk_kamar = d.getBanyakKamar();
+            int byk_malam = d.getBanyakMalam();
+            int total = d.getHargaTotal();
+            
+            String sql = "INSERT INTO detil_pemesanan VALUES ('%s','%s','%s','%s',%d,%d,%d)";
+            sql = String.format(sql, id, id_c, id_h, id_k, byk_kamar, byk_malam, total);
+            
+            stmt.execute(sql);
+            System.out.println("Detil pemesanan berhasil dibuat.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
