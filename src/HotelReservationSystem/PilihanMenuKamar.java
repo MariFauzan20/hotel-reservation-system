7f61665a4244afb426f0ad9861bf0d9a5e8ff026
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2022-01-01 15:09:55 +0700

    Add PilihanMenuKamar.java

---
+++ src/HotelReservationSystem/PilihanMenuKamar.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HotelReservationSystem;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author Muhammad Tsaqif Ammar
+ */
+public class PilihanMenuKamar {
+    private String id;
+    private Kamar jenisKamar;
+    private int banyakKamar;
+    
+    public PilihanMenuKamar(Kamar jenisKamar, int banyakKamar) {
+        this.id = UUID.randomUUID().toString();
+        this.jenisKamar = jenisKamar;
+        this.banyakKamar = banyakKamar;
+    }
+    
+    public void setJenisKamar(Kamar kamar) {
+        this.jenisKamar = kamar;
+    }
+    public void setbanyakKamar(int banyakKamar) {
+        this.banyakKamar = banyakKamar;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    public Kamar getJenisKamar() {
+        return jenisKamar;
+    }
+    public int getbanyakKamar() {
+        return banyakKamar;
+    }
+    
+    public void tambahKamar(int num) {
+        banyakKamar += num;
+    }
+    public void kurangKamar(int num) {
+        banyakKamar -= num;
+    }
+}
